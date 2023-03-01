09157f82b9d0faabb17115f71797a5bb5cf1a482
benkabundi01 <benkabundi01@gmail.com>
2023-03-01 12:51:21 -0500

    Changed inquirer version

--- index.js
+++ index.js
@@ -32,67 +32,67 @@ function startEmployeeTracker() {
         switch (action) {
             case "View All Employess":
                 console.log("Now Viewing All Employess: \n");
-
+                viewingAllEmployees();
                 break;
 
             case "View All Employees By Manager":
                 console.log("Now Viewing All Employees By Manager: \n");
-
+                employeesByManager();
                 break;
 
             case "View All Employees By Department":
                 console.log("Now Viewing All Employess By Department: \n");
-                
+                employeesByDepartment();
                 break;
 
             case "Add An Employee":
                 console.log("Now Adding An Employee: \n");
-                
+                addingEmployee();
                 break;
 
             case "Update An Employee Role":
                 console.log("Now Updating An Employee Role: \n");
-                
+                updateEmployeeRole();
                 break;
 
             case "View All Departments":
                 console.log("Now Viewing All Departments: \n");
-                
+                viewAllDepartments();
                 break;
 
             case "Add A Department":
                 console.log("Now Adding A Department: \n");
-                
+                addDepartment();
                 break;
 
             case "View All Roles":
                 console.log("Now Viewing View All Roles: \n");
-
+                viewAllRoles();
                 break;    
 
             case "Add A Role":
                 console.log("Now Adding A Role: \n");
-            
+                addRole();
                 break;    
                 
             case "Delete A Department":
                 console.log("Now Deleting A Department: \n");
-            
+                deleteDepartment();
                 break;    
 
             case "Delete A Role":
                 console.log("Now Deleting A Role: \n");
-            
+                deleteRole();
                 break;    
 
             case "Delete An Employee":
                 console.log("Now Deleting An Employee: \n");
-            
+                deleteEmployee();               
                 break;    
 
             case "View Department Budget":
                 console.log("Now Viewing Department Budget: \n");
-            
+                departmentBudget();
                 break;    
 
             case "All Done":
@@ -102,16 +102,15 @@ function startEmployeeTracker() {
 
             default:
                 console.log("Please Enter A Valid Selection!!");
-            
+                startEmployeeTracker();
         }
     });
 }
 
-//all the functions for the different selections
 
-let viewingAllEmployees = emplyees() {
-
-}
+/**
+ * Functions for all the different selections from the user
+ */
 
 //viewing employees by their managers
 function employeesByManager() {
@@ -136,6 +135,11 @@ function updateEmployeeRole() {
 //viewing all departments
 function viewAllDepartments() {
     
+}
+
+//adding a department
+function addDepartment() {
+
 }
 
 function viewAllRoles() {
