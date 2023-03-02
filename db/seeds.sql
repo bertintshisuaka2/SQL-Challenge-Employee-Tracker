8f8515dedffb03104c4074dcc974d5a0b7399b59
bertintshisuaka2 <bertintshisuaka@hotmail.com>
2023-03-01 23:33:25 -0500

    Some small changes to some files

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,13 +4,13 @@ VALUES ('Engineering'),
        ('Finance');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Software Developer', 100000, 1);
+VALUES ('Software Developer', 100000, 1),
        ('Marketing Analyst', 75000, 2), 
        ('Accountant', 80000, 3),
        ('Lead Developer', 120000, 1);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES ('Ash', 'Ketchum', 2, NULL),
-       ('Grace', 'kabuika', 4, NULL);
+       ('Grace', 'kabuika', 4, NULL),
        ('Ben', 'Tshisuaka', 1, 2),
        ('Bertin', 'Tshisuaka', 1, 2);
